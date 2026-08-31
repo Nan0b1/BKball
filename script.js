e680fc9f7a68088d15a461114974e5bb59a31526
24oulia <staff@webgarbage.frt>
2026-08-31 17:50:50 +0200

    Formatting

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 "use strict";
 
 function showKeyPress(evt) {
-    let char = String.fromCharCode(evt.charCode)
+    let char = String.fromCharCode(evt.charCode);
 
     if (char == "p") {
-        r = r + 2
+        r = r + 2;
     }
     if (char == "o") {
-        r = r - 2
+        r = r - 2;
     }
     if (char == "a") {
-        R = R + 2
+        R = R + 2;
     }
     if (char == "z") {
-        R = R - 2
+        R = R - 2;
     }
 
-    actualize()
+    actualize();
 }
 
 
@@ -28,18 +28,18 @@ let circle = document.getElementById("myCircle");
 
 // https://mathworld.wolfram.com/Circle-CircleIntersection.html<
 // (0,0) (0,140) -> d=140
-let d = 140
+let d = 140;
 // default lenght rope 100
-let R = 190 // gauche
-let r = 190 // droit
+let R = 190; // gauche
+let r = 190; // droit
 
 
 
-let x = 0
-let old_x = x
+let x = 0;
+let old_x = x;
 
-let y = 0
-let old_y = y
+let y = 0;
+let old_y = y;
 
 function changeSegment(name) {
     let segment = document.getElementById(name + "1");
@@ -51,7 +51,7 @@ function changeSegment(name) {
     segmenty.beginElement();
 }
 
-let holes = []
+let holes = [];
 
 function makeHole(coordonates, radius) {
     let newHole = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
@@ -66,13 +66,13 @@ function makeHole(coordonates, radius) {
     holes.push([coordonates,radius]);
 }
 
-var levels = []
-var level0 = [[[50,50],15], [[10,50],5], [[50,10],3], [[30,25],5]]
-levels.push(level0)
+var levels = [];
+var level0 = [[[50,50],15], [[10,50],5], [[50,10],3], [[30,25],5]];
+levels.push(level0);
 
 function makeLevel(levelID) {
     for (let i = 0; i < levels[levelID].length; i++) {
-        makeHole(levels[levelID][i][0],levels[levelID][i][1])
+        makeHole(levels[levelID][i][0],levels[levelID][i][1]);
     }
 }
 
@@ -81,7 +81,7 @@ makeLevel(0)
 
 function removeHoles(){
     for (let i = 0; i < holes.length; i++) {
-        console.log(holes[i].toString())
+        console.log(holes[i].toString());
         document.getElementById(holes[i].toString()).remove();
     }
 }
@@ -90,26 +90,26 @@ function removeHoles(){
 
 
 function actualize() {
-    old_x = x
-    x = ((d ** 2) - (r ** 2) + (R ** 2)) / (2 * d)
+    old_x = x;
+    x = ((d ** 2) - (r ** 2) + (R ** 2)) / (2 * d);
 
     old_y = y
-    let a = (1 / d) * ((-d + r - R) * (-d - r + R) * (-d + r + R) * (d + r + R)) ** (1 / 2)
-    y = a / 2
+    let a = (1 / d) * ((-d + r - R) * (-d - r + R) * (-d + r + R) * (d + r + R)) ** (1 / 2);
+    y = a / 2;
 
     if (x < 5) {
-        x = 5
+        x = 5;
     } 
     else if (x > 135) {
-        x = 135
+        x = 135;
     }
 
-    changeSegment("right1")
-    changeSegment("right2")
-    changeSegment("right3")
-    changeSegment("left1")
-    changeSegment("left2")
-    changeSegment("left3")
+    changeSegment("right1");
+    changeSegment("right2");
+    changeSegment("right3");
+    changeSegment("left1");
+    changeSegment("left2");
+    changeSegment("left3");
 
     let segment = document.getElementById("circleAnimate");
     segment.setAttribute("from", old_x + " " + old_y);
@@ -119,5 +119,5 @@ function actualize() {
 
 }
 
-actualize()
-actualize()
+actualize();
+actualize();
