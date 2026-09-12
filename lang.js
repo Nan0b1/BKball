d0702a50424e3f10f364486fe0d1816eaa7744d4
24oulia <staff@webgarbage.frt>
2026-09-12 19:04:06 +0200

    Implemented load_lang

--- lang.js
+++ lang.js
@@ -1,13 +1,36 @@
 "use strict";
 
 let lang = "en";
+const l_map = new Map();
+
+
+function load_lang(lang) {
+    fetch(`/langs/${lang}.txt`)
+    .then(response => response.text())
+    .then((data) => {
+        const lines = data.split("\n");
+        for (const line of lines) {
+            let temp = line.split(":");
+            let key = temp[0];
+            let value = temp[1];
+            l_map.set(key, value);
+        }
+    })
+
+}
 
 function updateLoc() {
-    let el = document.querySelector("#loc");
-
+    let els = document.querySelectorAll(".loc");
+    
+    for (const el of els) {
+        el.innerHTML = "a";
+    }
 
 }
 
 function updateElLoc(el) {
 
 }
+
+load_lang("en")
+updateLoc();
